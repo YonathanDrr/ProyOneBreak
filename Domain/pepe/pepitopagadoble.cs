be5c4d9b9a004a6e1788124563a28c08f6984f9a
Yonathan López Rosales <Y_Lopez@inn.intranet>
2021-12-16 16:32:41 -0300

    Pruebapepe

---
+++ Domain/pepe/pepitopagadoble.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.pepe
+{
+    class pepitopagadoble 
+    {
+        public int MyProperty { get; set; }
+    }
+}
